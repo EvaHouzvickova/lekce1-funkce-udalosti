fbb8ce677fcb8177afae687c49878223fd63e352
Eva Houžvičková <houzvickova@icloud.com>
2022-05-01 13:30:17 +0200

    ukol hotovy

--- main.js
+++ main.js
@@ -1,45 +1,29 @@
-// tady je místo pro náš program
-
-/**
- * Zobrazí výsledek sečtení dvou čísel. Na sečtení se využívá interní funkce.
- * 
- * @param {string} selector
- * @param {int} a 
- * @param {int} b 
- */
-function zobrazVysledek(selector, a, b) {
-	document.querySelector(selector).innerHTML = secti(a, b);
-}
-
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
- * 
- * @param {int} a 
- * @param {int} b 
+ *
+ * @param {int} a
+ * @param {int} b
  */
+
 function secti(a, b) {
 	let c = a + b;
 	return c;
 }
 
-/**
- * Upozorní uživatele při spuštění.
- */
-function upozorni() {
-	alert('Gratulace, právě si spustila tuto funkci!');
-	console.log('Gratulace, právě si spustila tuto funkci!');
+function zobrazVysledek() {
+  document.querySelector('#vysledek').innerHTML = secti(100, 50);
+};
 
-	let squareElement = document.getElementsByClassName('ctverecek')[0]
-	squareElement.innerHTML = 'Gratulace, právě si spustila tuto funkci!';
-	// Hmmm... dostali jsme se do problému s délkou řetězce a šířkou elementu. Pojďme to vyřešit.
-	squareElement.style.width = '400px';
+function upozorni() {
+  alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log('Gratulace, právě si spustila tuto funkci!');
+
+  let squareElement = document.getElementsByClassName('ctverecek')[0];
+  squareElement.innerHTML = 'Gratulace, právě jsi spustila tuto funkci!';
+  squareElement.style.width = '400px';
 }
 
-/**
- * Upraví barvu pozadí předaného prvku
- * 
- * @param {string} elementSelector 
- */
-function upravBarvu(elementSelector) {
-	document.querySelector(elementSelector).style.backgroundColor = 'green';
+function zmenaBarvy() {
+  let ctverecek = document.querySelector('.ctverecek');
+  ctverecek.style.backgroundColor = 'green';
 }
